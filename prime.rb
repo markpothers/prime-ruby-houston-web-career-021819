73d0ae51042ec84336a5c0a6fe5ff81e9fe83baa
Mark Pothecary <markpothers@hotmail.com>
2019-02-09 22:44:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(number)
+  array = []
+  i = 2
+    for i in i..number-1 do
+      array.push(i)
+      i += 1
+    end
+  x = 0
+  number = number.to_f
+  array.each do
+    if (number / array[x]).is_a? Integer
+      x += 1
+      return false
+    end
+  end
+  return true
+end
