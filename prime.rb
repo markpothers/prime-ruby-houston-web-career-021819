7b486716f19a3d0ffb465ec9c71f7d2ce24b5485
Mark Pothecary <markpothers@hotmail.com>
2019-02-09 22:55:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -11,6 +11,9 @@ def prime?(number)
   x = 0
   number = number.to_f
       array.each do
+        if number < 1
+          prime = false
+        end
         if number/array[x] == (number/array[x]).floor
           x += 1
           prime = false
